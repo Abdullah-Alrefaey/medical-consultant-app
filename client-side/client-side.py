6d2fccf3c0b092ebe8beb2f60cc2028252ef9714
Refaey <abdullah.m.alrefaey@gmail.com>
2021-01-17 22:35:04 +0200

    Added More Comments and Edits

--- client-side/client-side.py
+++ client-side/client-side.py
@@ -14,7 +14,9 @@ TIMEOUT_MESSAGE = "!TIMEOUT"                    # TIMEOUT_MESSAGE (if sent, the 
 
 
 class MedicalConsultantClient(m.Ui_MainWindow):
-
+    """
+    Main Class of the program GUI
+    """
     def __init__(self, starterWindow):
         """
         Main loop of the UI
@@ -22,15 +24,20 @@ class MedicalConsultantClient(m.Ui_MainWindow):
         """
         super(MedicalConsultantClient, self).setupUi(starterWindow)
 
-        # Setup
+        # Setup Button Connections
         self.connect_btn.clicked.connect(self.connect_server)
         self.disconnect_btn.clicked.connect(self.disconnect_server)
         self.client_message_text.returnPressed.connect(self.message_changed)
-        self.client_message_text.setDisabled(True)
-        self.disconnect_btn.setDisabled(True)
-        self.chat_area.setDisabled(True)
+        self.disableGUI()
 
     def connect_server(self):
+        """
+        This function responsible for:
+        1. Use SSL for socket security handling
+        2. Creating a socket object and connect the client to the server
+        3. Ensure that the connection is secure using the CA certificate
+        :return:
+        """
         self.client_name = self.client_name_text.text()
         self.receiver_name = self.receive_name_text.text()
         SERVER, PORT = self.host.text(), int(self.port.text())
@@ -55,44 +62,43 @@ class MedicalConsultantClient(m.Ui_MainWindow):
         # Connect to a given host/port
         try:
             self.client.connect(ADDR)
-
-            # Obtain the certificate from the server
-            server_cert = self.client.getpeercert()
-
-            # Validate whether the Certificate is indeed issued to the server
-            subject = dict(item[0] for item in server_cert['subject'])
-            commonName = subject['commonName']
-            print(commonName)
-            if not server_cert:
-                raise Exception("Unable to retrieve server certificate")
-
-            if commonName != 'Example-Root-CA':
-                raise Exception("Incorrect common name in server certificate")
-
-            notAfterTimestamp = ssl.cert_time_to_seconds(server_cert['notAfter'])
-            notBeforeTimestamp = ssl.cert_time_to_seconds(server_cert['notBefore'])
-            currentTimeStamp = time.time()
-
-            if currentTimeStamp > notAfterTimestamp:
-                raise Exception("Expired server certificate")
-
-            if currentTimeStamp < notBeforeTimestamp:
-                raise Exception("Server certificate not yet active")
-
         except socket.gaierror as e:
             print(f"Address-related error connecting to server: {e}")
             sys.exit(1)
-        except socket.error as e:
-            print(f"Connection error: {e}")
-            sys.exit(1)
+
+        if self.client:
+            try:
+                # Obtain the certificate from the server
+                server_cert = self.client.getpeercert()
+
+                # Validate whether the Certificate is indeed issued to the server
+                subject = dict(item[0] for item in server_cert['subject'])
+                commonName = subject['commonName']
+                print(commonName)
+                if not server_cert:
+                    raise Exception("Unable to retrieve server certificate")
+
+                if commonName != 'Example-Root-CA':
+                    raise Exception("Incorrect common name in server certificate")
+
+                notAfterTimestamp = ssl.cert_time_to_seconds(server_cert['notAfter'])
+                notBeforeTimestamp = ssl.cert_time_to_seconds(server_cert['notBefore'])
+                currentTimeStamp = time.time()
+
+                if currentTimeStamp > notAfterTimestamp:
+                    raise Exception("Expired server certificate")
+
+                if currentTimeStamp < notBeforeTimestamp:
+                    raise Exception("Server certificate not yet active")
+
+            except socket.error as e:
+                print(f"Connection error: {e}")
+                sys.exit(1)
 
         # If Connected Successfully
         self.status_label.setText("Connected To Successfully!")
         self.server_message_text.setText("...")
-        self.client_message_text.setDisabled(False)
-        self.disconnect_btn.setDisabled(False)
-        self.chat_area.setDisabled(False)
-        self.chat_area.setReadOnly(True)
+        self.enableGUI()
 
         # Send Client and Receiver Name
         try:
@@ -108,30 +114,35 @@ class MedicalConsultantClient(m.Ui_MainWindow):
         self.client_timer = setInterval(0.3, self.handle_received_message)
 
     def disconnect_server(self):
+        """
+        This function responsible for:
+        1. Closing the connection and its related running processes
+        2. Clear the GUI and disable its functions
+        :return:
+        """
         self.send_message(DISCONNECT_MESSAGE)
         self.status_label.setText("Disconnect From Server!")
         self.server_message_text.setText("Bye, " + self.client_name_text.text())
-        self.client_message_text.setDisabled(True)
-        self.disconnect_btn.setDisabled(True)
-        self.chat_area.setDisabled(True)
         self.chat_area.clear()
         self.client_timer.cancel()
         self.client.close()
         self.clientSocket.close()
+        self.disableGUI()
+
 
     def handle_received_message(self):
         global received_message
         try:
+            # Receive any message coming from the server
             received_message = self.client.recv(2048).decode(FORMAT)
         except socket.error as e:
             print(f"Error receiving data: {e}")
             sys.exit(1)
+
         # socket was closed for some other reason
         except ConnectionResetError:
             self.status_label.setText("Server is Closed!")
-            self.client_message_text.setDisabled(True)
-            self.disconnect_btn.setDisabled(True)
-            self.chat_area.setDisabled(True)
+            self.disableGUI()
 
         # Handle TIMEOUT Connection
         if received_message == TIMEOUT_MESSAGE:
@@ -141,9 +152,7 @@ class MedicalConsultantClient(m.Ui_MainWindow):
             self.client = None
             self.server_message_text.setText(received_message)
             self.status_label.setText("Disconnect From Server!")
-            self.client_message_text.setDisabled(True)
-            self.disconnect_btn.setDisabled(True)
-            self.chat_area.setDisabled(True)
+            self.disableGUI()
         elif received_message[0] == "$":
             self.server_message_text.setText(received_message[1:])
         elif received_message == "User Not Authorized":
@@ -153,10 +162,9 @@ class MedicalConsultantClient(m.Ui_MainWindow):
             self.client = None
             self.server_message_text.setText(received_message)
             self.status_label.setText("Not Authorized")
-            self.client_message_text.setDisabled(True)
-            self.disconnect_btn.setDisabled(True)
-            self.chat_area.setDisabled(True)
+            self.disableGUI()
         else:
+            # The Actual message if user is authorized and connected
             self.received_message_text.setText(received_message)
 
             # Append Received Message to chat area on left side
@@ -164,12 +172,13 @@ class MedicalConsultantClient(m.Ui_MainWindow):
             self.chat_area.append(received_message)
             self.chat_area.setAlignment(QtCore.Qt.AlignLeft)
 
-            # self.chatting.setText(
-            #     '%s<p>%s</p>' % (self.chatting.text(), received_message))
-            # self.chatting.setAlignment(QtCore.Qt.AlignLeft)
-
 
     def message_changed(self):
+        """
+        This function handles sending message to server
+        If Enter is pressed in the Text Line Edit
+        :return:
+        """
         message = self.client_message_text.text()
 
         # Check if client disconnect from server or normal message
@@ -182,19 +191,40 @@ class MedicalConsultantClient(m.Ui_MainWindow):
             self.chat_area.setAlignment(QtCore.Qt.AlignRight)
             self.client_message_text.clear()
 
-
-            # self.chatting.setText(
-            #     '%s<p>%s</p>' % (self.chatting.text(), message))
-            # self.chatting.setAlignment(QtCore.Qt.AlignRight)
-
-
     def send_message(self, msg):
+        """
+        This function handles sending message header and the actual message to the server
+        :param msg: Message to be sent to the server
+        :return:
+        """
         message = msg.encode(FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
         send_length += b' ' * (HEADER - len(send_length))
         self.client.send(send_length)
         self.client.send(message)
+
+    def disableGUI(self):
+        """
+        This function disables the non-necessary elements in the GUI
+        if the user is not connected
+        :return:
+        """
+        self.client_message_text.setDisabled(True)
+        self.disconnect_btn.setDisabled(True)
+        self.chat_area.setDisabled(True)
+        self.chat_area.setReadOnly(True)
+
+    def enableGUI(self):
+        """
+        This function enables the necessary elements in the GUI
+        if the user is connected
+        :return:
+        """
+        self.client_message_text.setDisabled(False)
+        self.disconnect_btn.setDisabled(False)
+        self.chat_area.setDisabled(False)
+        self.chat_area.setReadOnly(True)
 
 
 # Class to creat an interval for specific function using Threads
--- server-side/server-side.py
+++ server-side/server-side.py
@@ -5,107 +5,38 @@ import ssl
 from tinydb import TinyDB, Query
 
 
-HEADER = 64                                                     # Number of Bytes for message header
-PORT = 5050                                                     # PORT Number that the socket will listen to
-SERVER = socket.gethostbyname(socket.gethostname())             # Get IP (HOST) of local connected device
-ADDR = (SERVER, PORT)                                           # Address of socket to connect
-FORMAT = 'utf-8'                                                # Encoding and Decoding messages Format
-DISCONNECT_MESSAGE = "!DISCONNECT"                              # DISCONNECT_MESSAGE (if sent, the server will close connection of that client)
-TIMEOUT_MESSAGE = "!TIMEOUT"                    # TIMEOUT_MESSAGE (if sent, the server will close connection of that client)
-TIMEOUT_SECONDS = 180                                           # Number of seconds to wait before disconnect the idle client
-NUM_CLIENT = 0                                                  # Number of current clients
-clientsDB = {}                                                  # Dictionary to save clients
-db = TinyDB('Users.json')                                       # Database Object for Registered User
-User = Query()                                                  # Database Query for searching
+HEADER = 64                                             # Number of Bytes for message header
+PORT = 5050                                             # PORT Number that the socket will listen to
+SERVER = socket.gethostbyname(socket.gethostname())     # Get IP (HOST) of local connected device
+ADDR = (SERVER, PORT)                                   # Address of socket to connect
+FORMAT = 'utf-8'                                        # Encoding and Decoding messages Format
+DISCONNECT_MESSAGE = "!DISCONNECT"                      # DISCONNECT_MESSAGE (if sent, the server will close connection of that client)
+TIMEOUT_MESSAGE = "!TIMEOUT"                            # TIMEOUT_MESSAGE (if sent, the server will close connection of that client)
+TIMEOUT_SECONDS = 20                                    # Number of seconds to wait before disconnect the idle client
+NUM_CLIENT = 0                                          # Number of current clients
+clientsDB = {}                                          # Dictionary to save clients
+db = TinyDB('Users.json')                               # Database Object for Registered User
+User = Query()                                          # Database Query for searching
 
 
-def main():
-    # Create Socket Object and bind it to specific address
+def start_server():
+    """
+    This function responsible for:
+    1. Creating a new Socket Object (Server)
+    2. Bind the server to specific address
+    3. Make it a listening socket (enables a server to accept() connections)
+    :return:
+    """
+
+    global NUM_CLIENT
+
+    """
+    :param socket.AF_INET: address family (Internet address family for IPv4)
+    :param socket.SOCK_STREAM: Socket Type (TCP)
+    """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
-
-    # Starting Server
-    start_server(server)
-
-
-def disconnect_client(conn, addr, msg):
-    global NUM_CLIENT
-    print(f"[Disconnecting Client] {addr}")
-    conn.send(f"{msg}".encode(FORMAT))
-    conn.close()
-    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 2}")
-
-    # Remove client from clientsDB
-    clientsDB.pop(addr[1], None)
-    NUM_CLIENT -= 1
-
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        # Handle socket.timeout exception (raised by socket.settimeout()) 
-        try:
-            msg_length = conn.recv(HEADER).decode(FORMAT)
-        except socket.timeout:
-            disconnect_client(conn, addr, "!TIMEOUT")
-            break
-
-        # If socket received a message header successfully
-        if msg_length:
-            msg_length = int(msg_length)
-            
-            # Actual Message
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg:
-                # Save client name one time
-                if msg[0] == "@":
-                    clientsDB[addr[1]]['name'] = msg[1:]
-
-                # Save Receiver name one time
-                elif msg[0] == "#":
-                    clientsDB[addr[1]]['receiver'] = msg[1:]
-                    recv_name = clientsDB[addr[1]]['receiver']
-
-                elif msg == DISCONNECT_MESSAGE:
-                    disconnect_client(conn, addr, "Disconnect Client")
-                    connected = False
-                else:
-                    clientsDB[addr[1]]['messages'].append(msg)
-                    try:
-                        transfer_message_to_client(recv_name, msg)
-                        print(f"[{addr}][Msg Received] {msg}")
-                    except:
-                        raise Exception("Couldn't send message to client")
-
-                # Check if Client Finished Sending Initial Data
-                if clientsDB[addr[1]]['name'] and clientsDB[addr[1]]['receiver']:
-                    # Check if Name is in The DB, If Not Disconnect
-                    Result = db.search(User.name == clientsDB[addr[1]]['name'])
-                    if not Result:
-                        conn.send(f"User Not Authorized".encode(FORMAT))
-                        conn.close()
-                        print(f"[Disconnecting Client] {addr} {clientsDB[addr[1]]['name']}, Not Authorized")
-                        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-                        break
-                    else:
-                        # Send Message back to same client
-                        conn.send(f"$Welcome, {clientsDB[addr[1]]['name']}".encode(FORMAT))
-
-def transfer_message_to_client(receiverName, msg):
-    # Get the receiver who the client want to send message to
-    for key, value in clientsDB.items():
-        if value['name'] == receiverName:
-            print(f"found receiver: {receiverName}")
-            receiver_conn = value['connection']
-            receiver_conn.send(f"{msg}".encode(FORMAT))
-            break
-
-
-def start_server(server):
     print("[STARTING] server is starting...")
-    global NUM_CLIENT
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
 
@@ -123,22 +54,131 @@ def start_server(server):
                                ssl_version=ssl.PROTOCOL_TLSv1_2)
         conn.settimeout(TIMEOUT_SECONDS)
 
-        # Add the new Client to a dictionary
+        # Add the new Client to a dictionary (Volatile Database)
         NUM_CLIENT += 1
         new_client = {"id": NUM_CLIENT, "name": "", "receiver": "", "connection": conn, "address": addr, "messages": []}
         print(new_client)
-
-        # TODO
-        # Check if client Exists in Database
-        # If yes, create thread
-        # If no, don't create thread and close conn
         clientsDB[addr[1]] = new_client
+
         # Start a new thread for this Client
+        # Using Multiple threads to allow Multi-client connections.
+        # Each thread handles one client in a separate function
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
+        # Keep tracking how many users are connected to the socket.
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
+def handle_client(conn, addr):
+    """
+
+    :param conn: Socket Object (Client Connection)
+    :param addr: Address (IP, PORT Number) of the client
+    :return:
+    """
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # Handle socket.timeout exception (raised by socket.settimeout()) 
+        try:
+            # Receive the message header sent from client side
+            # This header is responsible for knowing the length of the actual incoming message
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+        except socket.timeout:
+            disconnect_client(conn, addr, "!TIMEOUT")
+            break
+
+        # If socket received a message header successfully
+        if msg_length:
+            msg_length = int(msg_length)
+            
+            # Receive The Actual Message
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg:
+                # Save client name one time
+                if msg[0] == "@":
+                    clientsDB[addr[1]]['name'] = msg[1:]
+
+                # Save Receiver name one time
+                elif msg[0] == "#":
+                    clientsDB[addr[1]]['receiver'] = msg[1:]
+
+                elif msg == DISCONNECT_MESSAGE:
+                    disconnect_client(conn, addr, "Disconnect Client")
+                    connected = False
+                else:
+                    # This case for the normal message
+                    clientsDB[addr[1]]['messages'].append(msg)
+
+                    try:
+                        transfer_message_to_client(clientsDB[addr[1]]['receiver'], msg)
+                        print(f"[{addr}][Msg Received] {msg}")
+                    except socket.error as e:
+                        print(f"Error client and receiver sending data: {e}")
+                        print("Couldn't send message to client")
+
+                # Check if Client Finished Sending Initial Data
+                if clientsDB[addr[1]]['name'] and clientsDB[addr[1]]['receiver']:
+                    # Check if Client Name Exists in The Database
+                    # If not exists, close the socket connection to disconnect the user
+                    result = db.search(User.name == clientsDB[addr[1]]['name'])
+                    if not result:
+                        conn.send(f"User Not Authorized".encode(FORMAT))
+                        conn.close()
+                        print(f"[Disconnecting Client] {addr} {clientsDB[addr[1]]['name']}, Not Authorized")
+                        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+                        break
+                    else:
+                        # Send Message back to same client
+                        try:
+                            conn.send(f"$Welcome, {clientsDB[addr[1]]['name']}".encode(FORMAT))
+                        except socket.error as e:
+                            print(f"Error client and receiver sending data: {e}")
+                            print("Couldn't send message to client")
+
+                # If the receiver is not connected
+                else:
+                    pass
+
+
+def disconnect_client(conn, addr, msg):
+    """
+
+    :param conn: Socket Object (Client Connection)
+    :param addr: Address (IP, PORT Number) of the client
+    :param msg: Message to be sent to user
+    :return:
+    """
+    global NUM_CLIENT
+    print(f"[Disconnecting Client] {addr}")
+    conn.send(f"{msg}".encode(FORMAT))
+    conn.close()
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 2}")
+
+    # Remove client from clientsDB
+    clientsDB.pop(addr[1], None)
+    NUM_CLIENT -= 1
+
+
+def transfer_message_to_client(receiver_name, msg):
+    """
+
+    :param receiver_name: The client name to send message to him
+    :param msg: The Actual Message to be sent
+    :return:
+    """
+
+    # Search in clientsDB to get the receiver who the client want to send message to
+    for key, value in clientsDB.items():
+        if value['name'] == receiver_name:
+            print(f"found receiver: {receiver_name}")
+            receiver_conn = value['connection']
+            receiver_conn.send(f"{msg}".encode(FORMAT))
+            break
+
+
 if __name__ == '__main__':
-    main()
+    # Start Main Program
+    start_server()
